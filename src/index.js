3bf6a6e09698565ccafad9ac9cf0edd2fe90f535
ZjVirus <285857527@qq.com>
2018-02-26 15:15:57 +0800

    国际化

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ import Main from './Main';
 import { Provider } from "react-redux";
 import store from "./Redux";
 import registerServiceWorker from './registerServiceWorker';
+import { LocaleProvider } from 'antd';
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
+// import 'moment/local/zh-cn';
 
 ReactDOM.render(
   <Provider store={store} >
-    <Main />
+    <LocaleProvider locale={zh_CN}>
+      <Main />
+    </LocaleProvider>
+    
   </Provider>,
   document.getElementById("root")
 );
