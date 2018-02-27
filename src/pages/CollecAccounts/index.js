03cccd88339cd2682fa04c074523f86aa8d6eaee
ZjVirus <285857527@qq.com>
2018-02-27 14:54:56 +0800

    藏品账目

--- src/pages/CollecAccounts/index.js
+++ src/pages/CollecAccounts/index.js
@@ -1,11 +1,89 @@
 import React, { Component } from 'react';
+import { Row, Col, DatePicker, Button, Table } from 'antd';
+import './index.less';
+const { RangePicker } = DatePicker;
 
 class CollecAccounts extends Component {
-    state = {  }
+    state = {  
+        accountData: [
+            {
+                key: 0,
+                date: '2018-02-27',
+                collectionNum: '5810',
+                dataNum: 6003,
+                replicaNum: 28,
+                imitationsNum: 389,
+                escrowNum: 12,
+                checkedOut: 51,
+                stayAccount: 344,
+                backLibrary: 51
+            }
+        ]
+    }
     render() {
-        return (
-            <div>藏品账目</div>
-        );
+        const { accountData } = this.state;
+        const accountsColumns = [
+            {
+                title: '时间',
+                dataIndex: 'date',
+                key: 'date'
+            },
+            {
+                title: '藏品数',
+                dataIndex: 'collectionNum',
+                key: 'collectionNum'
+            },
+            {
+                title: '资料数',
+                dataIndex: 'dateNum',
+                key: 'dataNum'
+            },
+            {
+                title: '复制品数',
+                dataIndex: 'replicaNum',
+                key: 'replicaNum'
+            },
+            {
+                title: '仿制品数',
+                dataIndex: 'imitationsNum',
+                key: 'initationsNum'
+            },
+            {
+                title: '代管数',
+                dataIndex: 'escrowNum',
+                key: 'escrowNum'
+            },
+            {
+                title: '外借数',
+                dataIndex: 'checkedOut',
+                key: 'checkedOut'
+            },
+            {
+                title: '待入账数',
+                dataIndex: 'stayAccount',
+                key: 'stayAccount'
+            },
+            {
+                title: '待回库数',
+                dataIndex: 'backLibrary',
+                key: 'backLibrary'
+            }
+        ]
+
+        return <Row className="main-content">
+            <Col span={24} className="title">
+              藏品账目列表
+            </Col>
+            <Col span={24} className="accounts-container">
+              <Col span={24} className='search-container' >
+                <RangePicker />
+                <Button type="primary">搜索</Button>
+              </Col>
+              <Col span={24}  >
+                <Table columns={accountsColumns} dataSource={accountData} bordered />
+              </Col>
+            </Col>
+          </Row>;
     }
 }
 
