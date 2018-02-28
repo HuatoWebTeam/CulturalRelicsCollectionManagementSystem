3cec534067eebe1f74335fae1e1ada1315ec2db0
ZjVirus <285857527@qq.com>
2018-02-28 16:54:15 +0800

    复仿制管理>藏品出库

---
+++ src/pages/ComplexGenericManage/ComplexOutbound/NewComplexOutbound.js
@@ -0,0 +1,178 @@
+import React, { Component } from 'react';
+import { Row, Col, Form, Button, Input, Table, Select, DatePicker } from 'antd';
+import './index.less';
+const FormItem = Form.Item;
+const Option = Select.Option;
+
+class NewComplexOutboundApp extends Component {
+  state = {
+    newOutboundData: [
+      {
+        key: 0,
+        relicsNum: "CHO46843",
+        rfid: "DH146543",
+        relicsImg: require("../../../assets/img/描金彩观音像.jpg"),
+        relicsName: "描金彩观音像",
+        localtion: "库房 1",
+        number: 1,
+        levelInfo: "普通藏品",
+        material: "陶器",
+        years: "唐",
+        howComplete: "破损"
+      }
+    ]
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, value) => {
+      if (!err) {
+        console.log(value);
+        // this.props.history.push("/App/Home");
+      }
+    });
+  };
+
+  render() {
+    const { newOutboundData } = this.state;
+    const { getFieldDecorator } = this.props.form;
+    const newOutboundColumns = [
+      {
+        title: "文物RFID",
+        dataIndex: "relicsNum",
+        key: "relicsNum"
+      },
+      {
+        title: "储柜RFID",
+        dataIndex: "rfid",
+        key: "rfid"
+      },
+      {
+        title: "文物图片",
+        dataIndex: "relicsImg",
+        key: "relicsImg",
+        render: (text, idx) => {
+          return (
+            <img
+              alt={idx}
+              src={text}
+              style={{ width: "55px", height: "55px" }}
+            />
+          );
+        }
+      },
+      {
+        title: "文物名称",
+        dataIndex: "relicsName",
+        key: "relicsName"
+      },
+      {
+        title: "储存位置",
+        dataIndex: "localtion",
+        key: "localtion"
+      },
+      {
+        title: "数量",
+        dataIndex: "number",
+        key: "number"
+      },
+      {
+        title: "分级信息",
+        dataIndex: "levelInfo",
+        key: "levelInfo"
+      },
+      {
+        title: "材质",
+        dataIndex: "material",
+        key: "material"
+      },
+      {
+        title: "年代",
+        dataIndex: "years",
+        key: "years"
+      },
+      {
+        title: "完整程度",
+        dataIndex: "howComplete",
+        key: "howComplete"
+      }
+    ];
+
+    return (
+      <Row className="main-content">
+        <Col span={24} className="title">
+          新建出库单
+        </Col>
+        <Col span={24} className="newOutbound-container">
+          <Form layout="inline" onSubmit={this.handleSubmit}>
+            <Col span={24} className="form-top">
+              <FormItem
+                label="出库用途:"
+                labelCol={{ span: 6 }}
+                className="form-item50"
+              >
+                {getFieldDecorator("outboundPurposes", {
+                  rules: [{ required: true, message: "请输入出库用途" }]
+                })(<Input />)}
+              </FormItem>
+              <FormItem
+                label="出库类型:"
+                labelCol={{ span: 6 }}
+                className="form-item50"
+              >
+                {getFieldDecorator("outboundPurposes", {
+                  rules: [{ required: true, message: "请选择出库类型" }]
+                })(
+                  <Select>
+                    <Option value="0">陈列</Option>
+                  </Select>
+                )}
+              </FormItem>
+              <FormItem
+                label="出库时间:"
+                labelCol={{ span: 6 }}
+                className="form-item50"
+              >
+                {getFieldDecorator("outboundPurposes", {
+                  rules: [{ required: true, message: "请选择出库时间" }]
+                })(<DatePicker />)}
+              </FormItem>
+              <FormItem
+                label="操作人:"
+                labelCol={{ span: 6 }}
+                className="form-item50"
+              >
+                {getFieldDecorator("outboundPurposes", {
+                  rules: [{ required: true, message: "请输入操作人" }]
+                })(<Input />)}
+              </FormItem>
+              <Button
+                type="primary"
+                style={{ marginLeft: "90px", marginBottom: "20px" }}
+              >
+                选择出库文物
+              </Button>
+            </Col>
+            <Col span={24}>
+              <Table
+                columns={newOutboundColumns}
+                dataSource={newOutboundData}
+                pagination={false}
+                bordered
+              />
+            </Col>
+            <Col span={24}>
+              <FormItem className="submitBtn">
+                <Button type="primary" htmlType="submit">
+                  提交出库单
+                </Button>
+              </FormItem>
+            </Col>
+          </Form>
+        </Col>
+      </Row>
+    );
+  }
+}
+const NewComplexOutbound = Form.create()(NewComplexOutboundApp);
+export default NewComplexOutbound;
--- src/pages/ComplexGenericManage/ComplexOutbound/index.js
+++ src/pages/ComplexGenericManage/ComplexOutbound/index.js
@@ -1,11 +1,141 @@
+
 import React, { Component } from 'react';
+import { Row, Col, Button, Input, DatePicker, Table } from 'antd';
+import './index.less';
+const Search = Input.Search;
+const { RangePicker } = DatePicker;
 
 class ComplexGeneric extends Component {
-    state = {  }
+    state = {  
+        outboundData: [
+            {
+                outboundNum: 'CK1756321',
+                relicsNum: 'Cp156',
+                relicsImg: require('../../../assets/img/描金彩观音像.jpg'),
+                relicsName: '描金彩观音像',
+                years: '唐',
+                number: 1,
+                date: '2018-02-26',
+                levelInfo: '普通藏品',
+                material: '陶器',
+                howComplete: '破损',
+                state: '在库',
+                category: '艺术',
+                size: '140',
+                weight: '6Kg',
+                key: 0
+            }
+        ]
+    }
+
+    handleSearch (e) {
+        console.log(e)
+    }
+
     render() {
-        return (
-            <div>藏品出库（复仿制）</div>
-        );
+        const { outboundData } = this.state;
+        const outboundColumns = [
+            {
+                title:'出库单号',
+                key: 'outboundNum',
+                dataIndex: 'outboundNum'
+            },
+            {
+                title: '文物编号',
+                dataIndex: 'relicsNum',
+                key: 'relicsNum'
+            },
+            {
+                title: '文物图片',
+                dataIndex: 'relicsImg',
+                key: 'relicsImg',
+                render: (text, idx) => {
+                    return (
+                        <img alt={idx} src={text} style={{width: '55px', height: '55px'}} />
+                    )
+                }
+            },
+            {
+                title: '文物名称',
+                dataIndex: 'relicsName',
+                key: 'relicsName'
+            },
+            {
+                title: '出库时间',
+                dataIndex: 'date',
+                key: 'date'
+            },
+            {
+                title: '数量',
+                dataIndex:'number',
+                key: 'number'
+            },
+            {
+                title: '分级信息',
+                dataIndex: 'levelInfo',
+                key: 'levelInfo'
+            },
+            {
+                title: '材质',
+                dataIndex: 'material',
+                key: 'material'
+            },
+            {
+                title: '年代',
+                dataIndex: 'years',
+                key: 'years'
+            },
+            {
+                title: '完整程度',
+                dataIndex: 'howComplete',
+                key: 'howComplete'
+            },
+            {
+                title: '状态',
+                dataIndex: 'state',
+                key: 'state'
+            },
+            {
+                title: '类别',
+                dataIndex:'category',
+                key: 'category'
+            },
+            {
+                title: '尺寸',
+                dataIndex: 'size',
+                key: 'size'
+            },
+            {
+                title: '重量',
+                dataIndex: 'weight',
+                key: 'weight'
+            }
+        ]
+
+        return <Row className="main-content">
+            <Col span={24} className="title">
+              藏品出库信息
+            </Col>
+            <Col span={24} className="outbound-container">
+              <Col span={24}>
+                <Col span={24}>
+                  <Button type="primary" icon="plus" onClick={() => { this.props.history.push("/App/NewComplexOutbound"); }} >
+                    新建出库单
+                  </Button>
+                </Col>
+                <Col span={24} style={{ padding: "20px 0" }}>
+                  <RangePicker />
+                  <Button type="primary" style={{ marginLeft: "20px" }}>
+                    搜索
+                  </Button>
+                  <Search enterButton onSearch={this.handleSearch} style={{ float: "right", width: "260px" }} />
+                </Col>
+                <Col span={24} >
+                    <Table columns={outboundColumns} dataSource={outboundData} bordered  />
+                </Col>
+              </Col>
+            </Col>
+          </Row>;
     }
 }
 
