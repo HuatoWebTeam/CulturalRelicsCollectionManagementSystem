d691ca1d3abb5f916108ef3ed1a61a9219b742aa
ZjVirus <285857527@qq.com>
2018-02-28 13:34:02 +0800

    login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import { Form, Icon, Input, Button  } from 'antd';
+import axios from 'axios';
 
 const FormItem = Form.Item;
 
 class LoginForm extends Component {
-  state = {};
+  state = {
+    ticket: 'AA43366BEA188F0C9B00907E9E22660603D6829EAE36C82B64719F5779D958EB27575838ADA2CF99CE98713D875EA3008E13A4420DED98CE357FA41EB14516BEB084BD6F08E915E3403C8D2602681018DF8CA43C01F3D997F39776F9F910A9E0FAB843BFF7D4E02302F61CC329A9DE8E'
+  };
   handleSubmit (e) {
     e.preventDefault();
     this.props.form.validateFields((err, value) => {
       if(!err) {
         console.log(value);
-        this.props.history.push('/App/Home');
+        axios.get('/api/User/LoginfoDesc', { headers: { 'Authorization': 'BasicAuth ' + this.state.ticket }}).then((res) => {
+          console.log(res);
+          this.props.history.push("/App/Home");
+        }).catch((err) => { console.log(err) })
+        
       }
     })
   }
