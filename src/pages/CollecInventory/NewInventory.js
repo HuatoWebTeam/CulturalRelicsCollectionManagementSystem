7b0bf0a4ebada7ef1885d501b83de79a9f05dd78
ZjVirus <285857527@qq.com>
2018-02-28 13:33:51 +0800

    藏品盘点

---
+++ src/pages/CollecInventory/NewInventory.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { Row, Col, Button, Form, Table, Input, DatePicker } from 'antd';
+import './index.less';
+const FormItem = Form.Item;
+
+class NewInventory extends Component {
+    state = {  
+      chooseInventoryRelics: [
+        {
+          key: 0,
+          date: '2018-02-28',
+          relicsWarehouse: '库房 1',
+          relicsName: '云纹扁足鼎',
+          relicsNumber:1,
+          type: '青铜',
+          levelInfo: '普通藏品',
+          inventoryPeople: '李四'
+        }
+      ]
+    }
+    render() {
+      const { chooseInventoryRelics } = this.state;
+      const inventoryColumns = [
+        {
+          title: '时间',
+          dataIndex: 'date',
+          key: 'date'
+        },
+        {
+          tutle: '文物库房',
+          dataIndex: 'relicsWarehouse',
+          key: 'relicsWarehpuse'
+        },
+        {
+          title: '文物名称',
+          dataIndex: 'relicsName',
+          key: 'relicsName'
+        },
+        {
+          title: '文物数量',
+          dataIndex: 'relicsNumber',
+          key: 'relicsNumber'
+        },
+        {
+          title: '类型',
+          dataIndex: 'type',
+          key: 'type'
+        },
+        {
+          title: '分级信息',
+          dataIndex: 'levelInfo',
+          key: 'levelInfo'
+        },
+        {
+          title: '盘点人',
+          dataIndex: 'inventoryPeople',
+          key: 'inventoryPeople'
+        }
+      ]
+
+        return <Row className="main-content">
+            <Col span={24} className="title">
+              新建盘点单
+            </Col>
+            <Col span={24} className="newInventory-container">
+              <Form layout="inline">
+                <Col span={24} style={{ width: "710px" }}>
+                  <FormItem label="盘点时间:" className="form-width50" >
+                    <DatePicker />
+                  </FormItem>
+                  <FormItem label="盘点人:" className="form-width50" >
+                    <Input />
+                  </FormItem>
+                  <Col span={24} style={{ padding: "20px 70px" }}>
+                    <Button type="primary">选择盘点文物</Button>
+                  </Col>
+                </Col>
+                <Col span={24} >
+                  <Table columns={inventoryColumns} dataSource={chooseInventoryRelics} bordered pagination={false} />
+                </Col>
+                <Col span={24} style={{padding: '20px 40px'}} >
+                  <Button type='primary' style={{float: 'right'}} >提交盘点单</Button>
+                </Col>
+              </Form>
+            </Col>
+          </Row>;
+    }
+}
+
+export default NewInventory;
--- src/pages/CollecInventory/index.js
+++ src/pages/CollecInventory/index.js
@@ -1,10 +1,112 @@
 import React, { Component } from 'react';
+import { Row, Col, Select, DatePicker, Input, Button, Form, Table } from 'antd';
+import './index.less';
+const Option = Select.Option;
+const { RangePicker } = DatePicker;
+const Search = Input.Search;
+const FormItem = Form.Item;
 
 class CollecInventory extends Component {
-    state = {  }
+    state = {  
+        inventoryDataList: [
+            {
+                key: 0,
+                inventoryNum: 'PD464321',
+                date: '2018-02-27',
+                relicsWarehouse: '库房1',
+                relicsName: '描金彩观音像',
+                number: 1,
+                type: '陶器',
+                levelInfo: '普通藏品',
+                inventoryPeople: '李四',
+                state: '待盘点'
+            }
+        ]
+    }
     render() {
+        const { inventoryDataList } = this.state;
+        const inventoryColumns = [
+            {
+                title: '盘点单号',
+                dataIndex: 'inventoryNum',
+                key: 'inventoryNum'
+            },
+            {
+                title: '时间',
+                dataIndex: 'date',
+                key: 'date'
+            },
+            {
+                title: '文物库房',
+                dataIndex: 'relicsWarehouse',
+                key: 'relicsWarehouse'
+            },
+            {
+                title: '文物名称',
+                dataIndex: 'relicsName',
+                key: 'relicsName'
+            },
+            {
+                title: '文物数量',
+                dataIndex: 'number',
+                key: 'number'
+            },
+            {
+                title: '类型',
+                dataIndex: 'type',
+                key: 'type'
+            },
+            {
+                title: '分级信息',
+                dataIndex: 'levelInfo',
+                key: 'levelInfo'
+            },
+            {
+                title: '盘点人',
+                dataIndex: 'inventoryPeople',
+                key: 'inventoryPeople'
+            },
+            {
+                title: '状态',
+                dataIndex: 'state',
+                key: 'state'
+            }
+        ]
+
         return (
-            <div>藏品盘点</div>
+            <Row className='main-content' >
+                <Col span={24} className='title' >
+                    藏品盘点列表
+                </Col>
+                <Col span={24} className='inventory-container' >
+                    <Col span={24} style={{marginBottom: '20px'}} >
+                        <Col span={24} style={{paddingBottom: '20px'}} >
+                            <Button type='primary' icon='plus' onClick={() => { this.props.history.push("/App/NewInventory"); }} >新建盘点单</Button>
+                        </Col>
+                        <Col span={18} >
+                            <Form layout='inline' >
+                                <FormItem label='文物库房:' className='select-form-item' >
+                                    <Select>
+                                        <Option value='0' >全部</Option>
+                                    </Select>
+                                </FormItem>
+                                <FormItem>
+                                    <RangePicker/>
+                                </FormItem>
+                                <FormItem>
+                                    <Button type='primary' >搜索</Button>
+                                </FormItem>
+                            </Form>
+                        </Col>
+                        <Col span={6} >
+                            <Search enterButton />
+                        </Col>
+                    </Col>
+                    <Col span={24} >
+                        <Table columns={inventoryColumns} dataSource={inventoryDataList} bordered />
+                    </Col>
+                </Col>
+            </Row>
         );
     }
 }
