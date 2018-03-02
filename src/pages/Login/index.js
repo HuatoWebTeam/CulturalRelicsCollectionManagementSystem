ab98b90059bbe3b530e78244bf4faa5197b76bb0
ZjVirus <285857527@qq.com>
2018-03-02 16:01:15 +0800

    登录页面

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,52 +1,92 @@
 import React, { Component } from 'react';
-import { Form, Icon, Input, Button  } from 'antd';
+import { Row, Col, Form, Icon, Input, Button  } from 'antd';
 import axios from 'axios';
 import VerifyCode from "../Components/VerificationCode";
+import './index.less';
 
 const FormItem = Form.Item;
 
 class LoginForm extends Component {
   state = {
-    ticket: 'AA43366BEA188F0C9B00907E9E22660603D6829EAE36C82B64719F5779D958EB27575838ADA2CF99CE98713D875EA3008E13A4420DED98CE357FA41EB14516BEB084BD6F08E915E3403C8D2602681018DF8CA43C01F3D997F39776F9F910A9E0FAB843BFF7D4E02302F61CC329A9DE8E'
+    ticket: 'AA43366BEA188F0C9B00907E9E22660603D6829EAE36C82B64719F5779D958EB27575838ADA2CF99CE98713D875EA3008E13A4420DED98CE357FA41EB14516BEB084BD6F08E915E3403C8D2602681018DF8CA43C01F3D997F39776F9F910A9E0FAB843BFF7D4E02302F61CC329A9DE8E',
+    verCode: ''
   };
   handleSubmit (e) {
     e.preventDefault();
     this.props.form.validateFields((err, value) => {
       if(!err) {
         console.log(value);
-        axios.get('/api/User/LoginfoDesc', { headers: { 'Authorization': 'BasicAuth ' + this.state.ticket }}).then((res) => {
-          console.log(res);
-          this.props.history.push("/App/Home");
-        }).catch((err) => { console.log(err) })
-        
+      //   axios.get('/api/User/LoginfoDesc', { headers: { 'Authorization': 'BasicAuth ' + this.state.ticket }}).then((res) => {
+      //     console.log(res);
+      //     this.props.history.push("/App/Home");
+      //   }).catch((err) => { console.log(err) })
+        this.props.history.push("/App/Home");
+        let userAuth = JSON.stringify({name: value.userName});
+        sessionStorage.setItem('user', userAuth);
+
       }
+
     })
   }
+
+  componentDidMount () {
+    console.log(this.refs);
+    
+  }
+
+  handleVerifyCode = (rule, value, callback) => {
+    // console.log(rule, value);
+    const { getFieldValue } = this.props.form;
+    if (value.toLowerCase() !== this.state.verCode) {
+      callback('验证码输入错误');
+    };
+    callback();
+  }
+
   render() {
-    console.log(this.props);
+    // console.log(this.props);
     const { getFieldDecorator } = this.props.form;
-    return (
-      <Form onSubmit={this.handleSubmit.bind(this)}>
-        <FormItem>
-          {getFieldDecorator("userName", {
-            rules: [{ required: true, message: "请输入用户名" }]
-          })(<Input placeholder="用户名" />)}
-        </FormItem>
-        <FormItem>
-          {getFieldDecorator("password", {
-            rules: [{ required: true, message: "请输入密码" }]
-          })(<Input type="password" placeholder="密码" />)}
-        </FormItem>
-        <FormItem>
-          <VerifyCode/>
-        </FormItem>
-        <FormItem>
-          <Button htmlType="submit" type="primary">
-            登录
-          </Button>
-        </FormItem>
-      </Form>
-    );
+    return <Row className="login-container">
+        <Col span={24} className="login-background" />
+        <Col span={24} className="login-form">
+          <Form onSubmit={this.handleSubmit.bind(this)}>
+            <FormItem>
+              <Col span={24}>
+                <h2 style={{ textAlign: "center" }}>用户登录</h2>
+              </Col>
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator("userName", {
+                rules: [{ required: true, message: "请输入用户名" }]
+              })(<Input prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)", fontSize: "18px" }} />} placeholder="请输入用户名" />)}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator("password", {
+                rules: [{ required: true, message: "请输入密码" }]
+              })(<Input prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)", fontSize: "18px" }} />} type="password" placeholder="请输入密码" />)}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator("verifyCode", {
+                rules: [
+                  {
+                    validator: this.handleVerifyCode
+                  }
+                ]
+              })(<Input placeholder="请输入验证码" prefix={<Icon type="safety" style={{ color: "rgba(0,0,0,.25)", fontSize: "18px" }} />} style={{ width: "50%", marginRight: "15px" }} />)}
+
+              <VerifyCode onChange={value => {
+                  console.log(value);
+                  this.setState({ verCode: value.toLowerCase() });
+                }} height={40} width={120} />
+            </FormItem>
+            <FormItem>
+              <Button htmlType="submit" type="primary" style={{ width: "100%", height: "40px" }}>
+                登录
+              </Button>
+            </FormItem>
+          </Form>
+        </Col>
+      </Row>;
   }
 }
 const Login = Form.create()(LoginForm);
