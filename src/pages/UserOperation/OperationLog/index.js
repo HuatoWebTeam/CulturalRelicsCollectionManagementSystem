69c2448dd190f03670794e2a328ace69c2bddb36
ZjVirus <285857527@qq.com>
2018-03-02 16:01:00 +0800

    用户操作日志

--- src/pages/UserOperation/OperationLog/index.js
+++ src/pages/UserOperation/OperationLog/index.js
@@ -1,10 +1,64 @@
 import React, { Component } from 'react';
+import { Row, Col, DatePicker, Button, Table } from 'antd';
+const { RangePicker } = DatePicker;
 
 class UserOperation extends Component {
-    state = {  }
+    state = {  
+        operationLogList: [
+            {
+                key: 0,
+                operationDate: '2018-03-02',
+                userName: '李四',
+                operationIp: '192.168.10.0',
+                operationType: '添加文物',
+                operationLog: '添加普通藏品黑石号文物'
+            }
+        ]
+    }
     render() {
+        const { operationLogList } = this.state;
+        const operationColumns = [
+            {
+                title: '操作时间',
+                dataIndex: 'operationDate',
+                key: 'operationDate'
+            },
+            {
+                title: '用户名',
+                dataIndex: 'userName',
+                key: 'userName'
+            },
+            {
+                title: '操作IP地址',
+                dataIndex: 'operationIP',
+                key: 'operationIP'
+            },
+            {
+                title: '操作类型',
+                dataIndex: 'operationType',
+                key: 'operationType'
+            },
+            {
+                title: '日志',
+                dataIndex: 'operationLog',
+                key: 'operationLog'
+            }
+        ]
         return (
-            <div>用户操作日志</div>
+            <Row className='main-content' >
+                <Col span={24} className='title' >
+                    操作日志列表
+                </Col>
+                <Col className='userOperation-container' span={24} style={{padding: '20px 40px 20px 20px'}} >
+                    <Col span={24} style={{paddingBottom: '20px'}} >
+                        <RangePicker  />
+                        <Button type='primary' style={{marginLeft: '20px'}} >搜索</Button>
+                    </Col>
+                    <Col span={24} >
+                        <Table columns={operationColumns} dataSource={operationLogList} bordered />
+                    </Col>
+                </Col>
+            </Row>
         );
     }
 }
