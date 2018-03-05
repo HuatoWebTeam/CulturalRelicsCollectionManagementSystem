0363c38e76b6d67325648e513be00f4c9e9e2df4
ZjVirus <285857527@qq.com>
2018-03-05 11:14:11 +0800

    权限分配

---
+++ src/pages/UserOperation/AssignPermissions.js/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'antd';
+
+class AssignPermissions extends Component {
+    state = {  }
+    render() {
+        return (
+            <Row className='main-content' >
+                <Col span={24} className='title' >权限分配</Col>
+            </Row>
+        );
+    }
+}
+
+export default AssignPermissions;
