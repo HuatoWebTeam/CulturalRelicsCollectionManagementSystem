f8399df59e5deac26539b487c33aba6bd04680fc
ZjVirus <285857527@qq.com>
2018-03-14 17:02:22 +0800

    添加路由

--- src/Router/index.js
+++ src/Router/index.js
@@ -15,7 +15,10 @@ const CollecExhibition = asyncComponent(() => import('../pages/CollecExhibition'
 const ExhibitionDetails = asyncComponent(() => import('../pages/CollecExhibition/ExhibitionDetails'));     // 藏品展览详情
 const AddExhibition = asyncComponent(() => import('../pages/CollecExhibition/AddExhibition'));              // 添加展览    
 const CollecRepair = asyncComponent(() => import('../pages/CollecRepair'));                                 // 修复    
-const NewRepairList = asyncComponent(() => import('../pages/CollecRepair/NewRepairList'));                  // 新建修复
+const NewRepairList = asyncComponent(() => import('../pages/CollecRepair/NewRepairList'));                  // 新建修复 
+const RepairDetails = asyncComponent(() =>
+  import("../pages/CollecRepair/RepairDetails")
+);                  // 修复单详情
 const Information = asyncComponent(() => import('../pages/CollecManagement/Information'));                  // 信息登记
 const AddRelics = asyncComponent(() => import('../pages/CollecManagement/Information/addRelics'));          // 新增藏品
 const Credentials = asyncComponent(() => import('../pages/CollecManagement/Credentials'));                  // 凭证信息列表
@@ -26,7 +29,7 @@ const NewOutbound = asyncComponent(() => import('../pages/CollecManagement/Outbo
 const Accounts = asyncComponent(() => import('../pages/CollecAccounts'));                                   // 藏品账目
 const Inventory = asyncComponent(() => import('../pages/CollecInventory'));                                 // 藏品盘点
 const NewInventory = asyncComponent(() => import('../pages/CollecInventory/NewInventory'));                 // 新建盘点
-const ShowDetails = asyncComponent(() => import('../pages/CollecInventory/ShowDetails'));                 // 盘点详情
+const ShowDetails = asyncComponent(() => import('../pages/CollecInventory/ShowDetails'));                   // 盘点详情
 const Solicition = asyncComponent(() => import('../pages/CollecSolicition'));                               // 藏品征集
 const AddSolicition = asyncComponent(() => import('../pages/CollecSolicition/AddSolicition'));                 // 藏品征集
 const ComlexInfo = asyncComponent(() => import('../pages/ComplexGenericManage/ComplexInfo'));               // 复仿制---信息登记
@@ -55,6 +58,7 @@ class Routes extends Component {
             <Route exact path="/App/AddExhibition" component={AddExhibition} />
             <Route exact path="/App/CollecRepair" component={CollecRepair} />
             <Route exact path="/App/NewRepairList" component={NewRepairList} />
+            <Route exact path="/App/RepairDetails/:id" component={RepairDetails} />
             <Route exact path="/App/Information" component={Information} />
             <Route exact path="/App/AddRelics" component={AddRelics} />
             <Route exact path="/App/Credentials" component={Credentials} />
