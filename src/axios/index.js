145bdd8a7c7ba90eb56cc957376e7721d7fb6a84
ZjVirus <285857527@qq.com>
2018-03-28 16:26:45 +0800

    添加接口

--- src/axios/index.js
+++ src/axios/index.js
@@ -2,3 +2,18 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = '/';
+
+// //  审批通过数据接口
+export const ApprovalPassed = (params) => {
+    return axios.post("/ApprovalFunctionManage/ApprovalPassed", params).then(res => res.data)
+}
+
+// 审批拒绝接口
+export const ApprovalDenied = (params) => {
+    return axios.post("/ApprovalFunctionManage/ApprovalDenied", params).then(res => res.data)
+}
+
+// 修改密码
+export const UpdateUserPwd = (params) => {
+    return axios.post("/UserManage/UpdateUserPwd", params).then(res => res.data)
+}
