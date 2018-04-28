cfeae719c28019ce96c0c70c434b75031a73a72c
ZjVirus <285857527@qq.com>
2018-04-28 11:22:36 +0800

    添加接口

--- src/axios/index.js
+++ src/axios/index.js
@@ -17,3 +17,28 @@ export const ApprovalDenied = (params) => {
 export const UpdateUserPwd = (params) => {
     return axios.post("/UserManage/UpdateUserPwd", params).then(res => res.data)
 }
+
+// 审批详情
+export const GetApprovalDetails = () => {
+    return axios.post("/GetApprovalDeta/GetApprovalDetails").then(res => res.data)
+}
+
+// 获取文物年代
+export const GetBasicDataOfTheAge = () => {
+    return axios.post("/BasicData/GetBasicDataOfTheAge").then(res => res.data);
+}
+// 获取文物类别
+export const GetBasicDataOfTypeOfCulturalRelic = () => {
+    return axios.post("/BasicData/GetBasicDataOfTypeOfCulturalRelic").then(res => res.data)
+}
+
+// 获取文物分级信息
+export const GetGradedInformationBaseData = () =>{
+    return axios.post("/BasicData/GetGradedInformationBaseData").then(res => res.data);
+}
+
+
+// 归还按钮
+export const ReturnAdd = (params) => {
+    return axios.post("Return/ReturnAdd", params).then(res => res.data)
+}
