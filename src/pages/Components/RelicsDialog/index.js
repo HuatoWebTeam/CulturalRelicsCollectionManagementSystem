75491737e786eb14f31af1acf8c982b43fce8c89
ZjVirus <285857527@qq.com>
2018-05-09 17:40:51 +0800

    修改选择文物组件

--- src/pages/Components/RelicsDialog/index.js
+++ src/pages/Components/RelicsDialog/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Col, Form, Select, Modal, Table, Input } from 'antd';
 import { StoreApi, ColleApi } from './api';
-import { levelInfo, relicsYears, relicsCategory, relicsState } from '../../../assets/js/commonFun';
+import { relicsState } from '../../../assets/js/commonFun';
 const FormItem = Form.Item;
 const Option = Select.Option;
 const { Search } = Input;
@@ -16,12 +16,24 @@ class RelicsDialog extends Component {
     searchCategory: "全部",
     searchName: "",
     searchRelicsData: [],
-    selectedRowKeys: []
+    selectedRowKeys: [],
+    relicsCateList: [],
+    relicsYearsList: [],
+    relicsLevelList: []
   };
 
   componentWillMount() {
+    console.log('willMount')
     console.log(this.props);
-    this.setState({ modalTitle: this.props.title });
+    let relicsYears = JSON.parse(sessionStorage.getItem("relicsYears"));
+    let relicsCateGory = JSON.parse(sessionStorage.getItem("relicsCateGory"));
+    let relicsLevel = JSON.parse(sessionStorage.getItem("relicsLevel"));
+    this.setState({
+      relicsYearsList: relicsYears,
+      relicsCateList: relicsCateGory,
+      relicsLevelList: relicsLevel,
+      modalTitle: this.props.title
+    });
     StoreApi().then(res => {
       // console.log(res);
       this.setState({
@@ -31,29 +43,40 @@ class RelicsDialog extends Component {
     this.searchModalData();
   }
   componentWillReceiveProps(nextProps) {
+    console.log('propsChange')
     console.log(nextProps);
     const { searchRelicsData } = this.state;
     // console.log(searchRelicsData)
     let item = nextProps.checkedItem;
     // console.log(item)
     let keys = [];
-    for(let value of item) {
+    for (let value of item) {
       keys.push(value.Collection_Number);
     }
     let data = searchRelicsData.concat(item);
+    for(let i = 0; i < data.length; i++) {
+      for(let n = i + 1; n < data.length; n++) {
+        if(data[i].Collection_Number === data[n].Collection_Number) {
+          data.splice(i, 1);
+          i--;
+          break;
+        }
+      }
+    }
     // console.log(data)
-    this.setState({
-      searchRelicsData: data,
-      selectedRowKeys: keys,
-    }, () => {
-      console.log(this.state)
-    })
+    this.setState(
+      {
+        searchRelicsData: data,
+        selectedRowKeys: keys
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
   }
 
   componentWillUnmount() {
-    this.setState({
-      searchRelicsData: []
-    })
+    this.setState({ searchRelicsData: [], selectedRowKeys: [] });
   }
 
   // 打开弹框
@@ -64,12 +87,15 @@ class RelicsDialog extends Component {
   //  弹窗搜索按钮
   chooseDataSearch = value => {
     // console.log(value);
-    this.setState({
-      searchName: value
-    }, () => {
-      this.searchModalData();
-    });
-    
+    this.setState(
+      {
+        searchName: value,
+        searchRelicsData: []
+      },
+      () => {
+        this.searchModalData();
+      }
+    );
   };
 
   searchModalData = () => {
@@ -84,11 +110,9 @@ class RelicsDialog extends Component {
     ColleApi(params).then(res => {
       console.log(res);
       const { searchRelicsData } = this.state;
-      let data = [];
+      // let data = [];
       for (let item of res) {
-        
-          item.key = item.Collection_Number;
-        
+        item.key = item.Collection_Number;
       }
       this.setState({
         searchRelicsData: res.concat(searchRelicsData)
@@ -127,9 +151,8 @@ class RelicsDialog extends Component {
   };
 
   onSelectChange = selectedRowKeys => {
-    
     this.setState({ selectedRowKeys });
-  }
+  };
 
   render() {
     const {
@@ -139,13 +162,14 @@ class RelicsDialog extends Component {
       modalTitle,
       searchStore,
       searchCategory,
-      selectedRowKeys
+      selectedRowKeys,
+      relicsCateList,
     } = this.state;
     // checkbox
     const rowSelection = {
       selectedRowKeys,
-      type: this.props.radio ? 'radio' : 'checkbox',
-      onChange: this.onSelectChange,
+      type: this.props.radio ? "radio" : "checkbox",
+      onChange: this.onSelectChange
     };
     // console.log(this.props)
     const chooseRelicsColumns = [
@@ -179,10 +203,10 @@ class RelicsDialog extends Component {
         dataIndex: "Collection_Name",
         width: 156
       },
-      {
-        title: "储存位置",
-        dataIndex: "Storage_Position",
-      },
+      // {
+      //   title: "储存位置",
+      //   dataIndex: "Storage_Position"
+      // },
       {
         title: "数量",
         dataIndex: "Number",
@@ -206,28 +230,21 @@ class RelicsDialog extends Component {
       {
         title: "完整程度",
         dataIndex: "Integrity",
-        render: (text) => {
-            if(Number(text) === 0) {
-                return (<span>完整</span>)
-            } else if(Number(text) === 1) {
-                return (<span>破损</span>)
-            }
+        render: text => {
+          if (Number(text) === 0) {
+            return <span>完整</span>;
+          } else if (Number(text) === 1) {
+            return <span>破损</span>;
+          }
         },
         width: 66
       },
       {
-          title: '文物状态',
-          dataIndex: 'Collection_State',
-          key: 'Collection_State',
-          render: (text) => {
-              for(let item of relicsState) {
-                  if(Number(text) === item.key) {
-                      return (<span style={{color: Number(text) === 5 ? 'red' : '#666'}} >{item.value}</span>)
-                  }
-              }
-          },
-          width: 66
-      },
+        title: "文物状态",
+        dataIndex: "CollStateName",
+        key: "CollStateName",
+        width: 66
+      }
     ];
     return (
       <Modal
@@ -255,11 +272,12 @@ class RelicsDialog extends Component {
                 }}
               >
                 <Option value={0}>全部</Option>
-                {relicsStoreList.length > 0 && relicsStoreList.map((item, idx) => (
-                  <Option value={item.Storehouse_Id} key={item.Storehouse_Id}>
-                    {item.Storehouse_Name}
-                  </Option>
-                ))}
+                {relicsStoreList.length > 0 &&
+                  relicsStoreList.map((item, idx) => (
+                    <Option value={item.Storehouse_Id} key={item.Storehouse_Id}>
+                      {item.Storehouse_Name}
+                    </Option>
+                  ))}
               </Select>
             </FormItem>
             <FormItem
@@ -267,7 +285,7 @@ class RelicsDialog extends Component {
               labelCol={{ span: 8 }}
               wrapperCol={{ span: 16 }}
             >
-            {/* <Input placeholder='请输入文物类别' 
+              {/* <Input placeholder='请输入文物类别' 
               value={searchCategory} 
               onChange={(e) => {
                 this.setState({
@@ -281,15 +299,19 @@ class RelicsDialog extends Component {
                 }}
               >
                 <Option value="全部">全部</Option>
-                {relicsCategory.map((item, idx) => (
-                  <Option value={item.key} key={item.key}>
-                    {item.value}
+                {relicsCateList.map((item, idx) => (
+                  <Option value={Number(item.CollTypeId)} key={Number(item.CollTypeId)}>
+                    {item.CollTypeName}
                   </Option>
                 ))}
               </Select>
             </FormItem>
             <FormItem labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
-              <Search placeholder='请输入文物名称' onSearch={this.chooseDataSearch} enterButton />
+              <Search
+                placeholder="请输入文物名称"
+                onSearch={this.chooseDataSearch}
+                enterButton
+              />
             </FormItem>
           </Form>
           <Col span={24}>
@@ -299,7 +321,7 @@ class RelicsDialog extends Component {
               columns={chooseRelicsColumns}
               pagination={false}
               bordered
-              scroll={{ x: 1150, y: 407}}
+              scroll={{ x: 1150, y: 407 }}
             />
           </Col>
         </Col>
