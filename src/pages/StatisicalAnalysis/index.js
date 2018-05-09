e0b1a46baea72d44d857fdf45c270e9c0a114c73
ZjVirus <285857527@qq.com>
2018-05-09 17:40:26 +0800

    修改数据统计

--- src/pages/StatisicalAnalysis/index.js
+++ src/pages/StatisicalAnalysis/index.js
@@ -10,8 +10,9 @@ import 'echarts/lib/chart/pie';
 // 引入提示框和标题组件
 import 'echarts/lib/component/tooltip';
 import 'echarts/lib/component/title';
+import moment from 'moment';
 
-import ReactEcharts from 'echarts-for-react';
+// import ReactEcharts from 'echarts-for-react';
 import { RangePickerDefault, subStr } from '../../assets/js/commonFun';
 import { GetStatisticalAnalysisData } from './api';
 const { RangePicker } = DatePicker;
@@ -21,12 +22,12 @@ const { RangePicker } = DatePicker;
 class Statisical extends Component {
   state = {
     data: [],
-    pieChartData:[],
+    pieChartData: [],
     date: RangePickerDefault,
     chartsDate: [],
     pieChartsData: [],
     chartLineWidth: 2,
-    format: 'YYYY-MM-DD',
+    format: "YYYY-MM-DD",
     allData: {}
   };
 
@@ -34,13 +35,15 @@ class Statisical extends Component {
     console.log(this.state);
     const { date, format } = this.state;
     console.log(date[0].format(format));
-    this.setState({
-      date: [date[0].format(format), date[1].format(format)]
-    }, () => {
+    this.setState(
+      {
+        date: [date[0].format(format), date[1].format(format)]
+      },
+      () => {
         this.getChartsData();
-    })
+      }
+    );
   }
-
 
   setChartsOption() {
     let lineChart = echarts.init(document.getElementById("lineCharts"));
@@ -49,44 +52,49 @@ class Statisical extends Component {
     pieChart.setOption(this.getPieChartOption());
   }
 
-  getChartsData () {
-    const { date } =this.state; 
+  // 禁止选择时间
+  disabledDate = current => {
+    return current && current > moment().endOf("day");
+  };
+  getChartsData() {
+    const { date } = this.state;
     let params = {
-        StaDate: date[0],
-        EndDate: date[1]
+      StaDate: date[0],
+      EndDate: date[1]
+    };
+    GetStatisticalAnalysisData(params).then(res => {
+      console.log(res);
+      let pieData = [
+        { name: "入库", value: res.Data.StorageNumber },
+        { name: "出库", value: res.Data.OutNumber },
+        { name: "征集", value: res.Data.CallNumber }
+      ];
+      let lineChart = [];
+      let chartsDate = [];
+
+      let chartsData = res.Data.ListIntermediateTable;
+      for (let item of chartsData) {
+        chartsDate.push(subStr(item.DateTime));
       }
-      GetStatisticalAnalysisData(params).then(res => {
-        console.log(res);
-        let pieData = [
-          { name: '入库', value: res.Data.StorageNumber },
-          { name: '出库', value: res.Data.OutNumber },
-          { name: '征集', value: res.Data.CallNumber }
-        ];
-        let lineChart = [];
-        let chartsDate = [];
-        
-        let chartsData = res.Data.ListIntermediateTable;
-        for(let item of chartsData) {
-          chartsDate.push(subStr(item.DateTime));
+      for (let n = 0; n < 3; n++) {
+        lineChart.splice(n, 1, {
+          name: n === 0 ? "入库" : n === 1 ? "出库" : "征集",
+          type: "line",
+          data: []
+        });
+        console.log(lineChart);
+        for (let i = 0; i < chartsData.length; i++) {
+          if (n === 0) {
+            lineChart[n].data.push(chartsData[i].StorageNumber);
+          } else if (n === 1) {
+            lineChart[n].data.push(chartsData[i].OutNumber);
+          } else {
+            lineChart[n].data.push(chartsData[i].CallNumber);
+          }
         }
-        for(let n = 0; n < 3; n++) {
-          lineChart.splice(n, 1, {
-            name: n === 0 ? '入库' : (n === 1 ? '出库' : '征集'),
-            type: 'line',
-            data: []
-          });
-          console.log(lineChart);
-          for (let i = 0; i < chartsData.length; i++) {
-            if(n === 0) {
-              lineChart[n].data.push(chartsData[i].StorageNumber);
-            } else if(n === 1) {
-              lineChart[n].data.push(chartsData[i].OutNumber);
-            } else {
-              lineChart[n].data.push(chartsData[i].CallNumber);
-            }
-          } 
-        }
-        this.setState({
+      }
+      this.setState(
+        {
           pieChartsData: pieData,
           chartsDate: chartsDate,
           data: lineChart,
@@ -95,17 +103,17 @@ class Statisical extends Component {
             OutNumber: res.Data.OutNumber,
             CallNumber: res.Data.CallNumber
           }
-        }, () =>{
+        },
+        () => {
           this.setChartsOption();
-        });
-        
-
-      })
+        }
+      );
+    });
   }
 
   getLineChartOption = () => {
-    const { chartsDate, data } = this.state;
-    console.log(chartsDate)
+    const { chartsDate } = this.state;
+    console.log(chartsDate);
     const option = {
       title: {
         text: ""
@@ -135,10 +143,9 @@ class Statisical extends Component {
         axisLabel: {
           formatter: function(value) {
             // console.log(value)
-            var str_before = value.split(' ')[0];
-            var str_after = value.split(' ')[1];
-            return  str_before;
-            
+            var str_before = value.split(" ")[0];
+            // var str_after = value.split(" ")[1];
+            return str_before;
           }
         }
       },
@@ -155,74 +162,90 @@ class Statisical extends Component {
 
   getPieChartOption = () => {
     const { pieChartsData } = this.state;
-    return  {
-    title : {
-        text: '',
-        subtext: '',
-        x:'center'
-    },
-    tooltip : {
-        trigger: 'item',
+    return {
+      title: {
+        text: "",
+        subtext: "",
+        x: "center"
+      },
+      tooltip: {
+        trigger: "item",
         formatter: "{a} <br/>{b} : {c} ({d}%)"
-    },
-    legend: {
-        orient: 'horizontal',
-        bottom: '0',
-        data: ['入库','出库','征集']
-    },
-    color: ["#3065bf", "#fabe55", "#ff875c"],
-    series : [
+      },
+      legend: {
+        orient: "horizontal",
+        bottom: "0",
+        data: ["入库", "出库", "征集"]
+      },
+      color: ["#3065bf", "#fabe55", "#ff875c"],
+      series: [
         {
-            name: '',
-            type: 'pie',
-            radius : '55%',
-            center: ['50%', '60%'],
-            data:pieChartsData,
-            itemStyle: {
-                emphasis: {
-                    shadowBlur: 10,
-                    shadowOffsetX: 0,
-                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-                }
+          name: "",
+          type: "pie",
+          radius: "55%",
+          center: ["50%", "60%"],
+          data: pieChartsData,
+          itemStyle: {
+            emphasis: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: "rgba(0, 0, 0, 0.5)"
             }
+          }
         }
-    ]
-};
-  }
+      ]
+    };
+  };
 
   render() {
     const { allData } = this.state;
 
-    return <Row className="main-content">
+    return (
+      <Row className="main-content">
         <Col span={24} className="title">
           统计分析图表
         </Col>
         <Col span={24} className="statisical-container">
           <Col span={24} style={{ paddingBottom: "20px" }}>
-            <RangePicker defaultValue={RangePickerDefault} format="YYYY-MM-DD" />
+            <RangePicker
+              defaultValue={RangePickerDefault}
+              format="YYYY-MM-DD"
+              disabledDate={this.disabledDate}
+            />
             <Button type="primary" style={{ marginLeft: "20px" }}>
               查询
             </Button>
           </Col>
           <Col span={24}>
             <Col span={16} style={{ textAlign: "center" }}>
-              <div id='lineCharts' style={{ width: "100%", height: "400px" }}  ></div>
+              <div id="lineCharts" style={{ width: "100%", height: "400px" }} />
               {/* <ReactEcharts style={{ width: "100%", height: "400px" }} option={this.getLineChartOption()} /> */}
-              <Col span={24} style={{ height: "30px", lineHeight: "30px", background: "#e8eef8" }}>
-                <Col span={8}>入库总数： {allData.StorageNumber}件</Col>
-                <Col span={8}>出库总数： {allData.OutNumber}件</Col>
-                <Col span={8}>征集总数： {allData.CallNumber}件</Col>
+              <Col
+                span={24}
+                style={{
+                  height: "30px",
+                  lineHeight: "30px",
+                  background: "#e8eef8"
+                }}
+              >
+                <Col span={8}>入库总数： {allData.StorageNumber} 件</Col>
+                <Col span={8}>出库总数： {allData.OutNumber} 件</Col>
+                <Col span={8}>征集总数： {allData.CallNumber} 件</Col>
               </Col>
             </Col>
             <Col span={8} style={{ padding: "40px 20px 0 20px" }}>
               <Col span={24} style={{ height: "390px", background: "#e8eef8" }}>
-                <div id='pieCharts' style={{ width: '100%', height: '350px' }} />
+                <div
+                  id="pieCharts"
+                  style={{ width: "100%", height: "350px" }}
+                />
                 {/* <ReactEcharts style={{ width: "100%", height: "350px" }} option={this.getPieChartOption()} /> */}
               </Col>
             </Col>
           </Col>
         </Col>
-      </Row>;
+      </Row>
+    );
   }
 }
 
